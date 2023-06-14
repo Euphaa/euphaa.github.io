5e766ead9fdce32630b0d4e844b231d280476377
Euphaa <135658782+Euphaa@users.noreply.github.com>
2023-06-14 16:01:32 -0400

    v0.2.1

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+function toggle_block(myDIV) {
+  var x = document.getElementById(myDIV);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
